23fa374e3d8991bf00bd2861da7cb2a7711e36ae
Maximilian Gerlach <m@px0.de>
2014-05-18 14:48:04 -0400

    initial commit

--- KlickTickets/MRGViewController.m
+++ KlickTickets/MRGViewController.m
@@ -7,9 +7,33 @@
 //
 
 #import "MRGViewController.h"
+#import "GenomeAuthenticator.h"
 
+//Ticket
+@interface Ticket : NSObject
+@property (nonatomic, copy) NSString *Title;
+@property (nonatomic, copy) NSString *TicketStatusName;
+@end
+
+@implementation Ticket
+@end
+
+//MappingProvider
+@interface MappingProvider : NSObject
++ (RKMapping *)ticketMapping;
+@end
+
+@implementation MappingProvider
++ (RKMapping *)ticketMapping {
+    RKObjectMapping *mapping = [RKObjectMapping mappingForClass:[Ticket class]];
+    [mapping addAttributeMappingsFromArray:@[@"Title", @"TicketStatusName"]];
+    return mapping;
+}
+@end
+
+//MRGViewController
 @interface MRGViewController ()
-
+@property (strong, nonatomic) GenomeAuthenticator *genomeAuthenticator;
 @end
 
 @implementation MRGViewController
@@ -26,4 +50,50 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+	[super viewDidAppear:animated];
+	
+	//authenticate
+	self.genomeAuthenticator = [[GenomeAuthenticator alloc] init];
+	@weakify(self);
+	[[_genomeAuthenticator authenticate] subscribeNext:^(id authStatus) {
+		BOOL authenticationSuccessful = [authStatus boolValue];
+		if(! authenticationSuccessful) {
+			@strongify(self);
+			[self presentViewController:self.genomeAuthenticator animated:YES completion:NULL];
+		}
+	} error:^(NSError *error) {
+		NSLog(@"An error has occured: %@", [error localizedDescription]);
+	} completed:^{
+		NSLog(@"You are now authenticated!");
+		[self getTickets];
+	}];
+	
+	//
+}
+
+- (void) getTickets {
+	NSIndexSet *statusCodeSet = RKStatusCodeIndexSetForClass(RKStatusCodeClassSuccessful);
+    RKMapping *mapping = [MappingProvider ticketMapping];
+	RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:mapping
+																							method:RKRequestMethodGET
+																					   pathPattern:nil
+																						   keyPath:@"Entries"
+																					   statusCodes:statusCodeSet];
+	
+    NSURL *url = [NSURL URLWithString:@"http://genome.klick.com:80/api/Ticket.json?ForGrid=true"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    RKObjectRequestOperation *operation = [[RKObjectRequestOperation alloc] initWithRequest:request
+                                                                        responseDescriptors:@[responseDescriptor]];
+    [operation setCompletionBlockWithSuccess:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {
+		for (Ticket *t in mappingResult.array) {
+			NSLog(@"t: %@ (%@)", t.Title, t.TicketStatusName);
+		}
+    } failure:^(RKObjectRequestOperation *operation, NSError *error) {
+        NSLog(@"ERROR: %@", error);
+        NSLog(@"Response: %@", operation.HTTPRequestOperation.responseString);
+    }];
+    
+    [operation start];
+}
 @end
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '7.0'
+inhibit_all_warnings!
+
+pod 'ReactiveCocoa'
+pod 'RestKit'
+pod 'ObjectiveSugar'
+pod 'QuickDialog'
+pod 'GenomeAuthenticator', :git => 'https://github.com/px0/GenomeAuthenticator'
